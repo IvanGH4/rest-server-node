6028f3dbe5736111c5c36c486290d13ad8b3db96
IvanGH4 <ivangarciaharriague07@gmail.com>
2021-01-27 12:57:54 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jakeP:0v3hvlUmst18ZJ5D@cluster0.ur0h6.mongodb.net/cafe';
+    urlDB = 'process.env.MONGO_URI';
 }
 
 process.env.URLDB = urlDB;
